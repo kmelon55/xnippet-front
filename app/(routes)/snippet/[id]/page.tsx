c2de26cc03d6de6a2077b43ff94a378562c6e4aa
Song <9567648@naver.com>
2024-03-23 23:35:15 +0900

    Feat: Implement dynamic routing for individual snippet pages

--- app/(routes)/snippet/[id]/page.tsx
+++ app/(routes)/snippet/[id]/page.tsx
@@ -1,21 +1,38 @@
-import SnippetDetail from "@/app/components/Editor/SnippetDetail";
-import SigninModalBtn from "@/app/components/Signin/SigninModalBtn";
+import SnippetEditor from "@/app/components/SnippetEditor";
 import { getSupabaseServerClient } from "@/app/services/supabase";
 
-export default async function SnippetPage() {
+export default async function SnippetPage({
+  params,
+}: {
+  params: { id: string };
+}) {
   const supabase = await getSupabaseServerClient();
-  const { data } = await supabase.auth.getSession();
-  const session = data.session;
+  const { data: sessionData } = await supabase.auth.getSession();
+  const session = sessionData.session;
+
+  const { data, error: snippetError } = await supabase
+    .from("snippet")
+    .select("*")
+    .eq("id", params.id)
+    .single();
+
+  if (snippetError) {
+    console.error(snippetError);
+    return <div>스니펫을 가져오는데 에러가 발생했습니다.</div>;
+  }
+
+  if (data.user_id !== session?.user.id) {
+    return <div>해당 스니펫을 볼 권한이 없습니다.</div>;
+  }
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-center">
-      <div className="flex gap-12 mt-3">
-        {session ? (
-          <SnippetDetail />
-        ) : (
-          <SigninModalBtn buttonText="로그인" buttonColor="primary" />
-        )}
-      </div>
+      <SnippetEditor
+        userId={data.user_id}
+        initialName={data.name}
+        initialCode={data.code}
+        snippetId={data.id}
+      />
     </div>
   );
 }
